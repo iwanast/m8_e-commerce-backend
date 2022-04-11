93648dcb8f6035d44ce1ced3cc8d79719a17d4a0
Iwana Städeli <78117193+iwanast@users.noreply.github.com>
2022-04-11 23:35:22 +0200

    basic set-up

---
+++ README.md
@@ -0,0 +1,3 @@
+Backend for the e-commerce module 8 in Hyper Island. 
+1. Run: npm install
+2. Run: npm run dev
---
+++ index.js
@@ -0,0 +1,5 @@
+import express from "express";
+import mongodb from "mongodb";
+import cors from "cors";
+
+console.log("Log is showing")

f8725e46356dbfa8dfdbea884f4d893f1d0ab7d0
Iwana Städeli <78117193+iwanast@users.noreply.github.com>
2022-04-12 15:53:11 +0200

    set-up mongoDB-connection and basic express

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 import express from "express";
 import mongodb from "mongodb";
 import cors from "cors";
+import dotenv from "dotenv";
 
-console.log("Log is showing")
+dotenv.config()
+
+const MONGODB_URL = process.env.MONGODB_URL
+const PORT = process.env.PORT
+const mongoClient = new mongodb.MongoClient(MONGODB_URL);
+mongoClient.connect();
+const dataBase = mongoClient.db("m8-e-commerce");
+const colApartment = dataBase.collection("apartments");
+
+
+const app = express();
+
+app.use(cors({origin: "http://localhost:3000"}));
+app.use(express.json());
+
+app.get("/apartment", async (req, res) =>{
+  try{
+    const apartments = await colApartment.find({}).toArray();
+    return res.json(apartments).end()
+  } catch(error){
+    console.log("Something went wrong when loading the apartments, bear with us");
+    res.sendStatus(500)
+  }
+})
+
+
+
+
+app.listen(PORT, () => console.log(`m8-e-commerce is running @ http://localhost:${PORT}`))
